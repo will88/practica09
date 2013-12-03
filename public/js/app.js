dce38fdb9043000256d6e163c58c04e4214a0db3
Diego Williams Aguilar Montaño <alu0100592368@ull.edu.es>
2013-12-03 20:56:42 +0000

    cambiando movimiento ilegal

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
     $(".cell").click(function(event) {
         var pathname = window.location.pathname;
         var ruta = pathname + event.target.id;
+        $.get(ruta, function(data) {
+            if (data == 'illegal'){
+                alert("Illegal move!");
+                } 
+            else if (data.length < 3)  {
+                if (data.length > 0){
+                    $("#"+event.target.id).addClass("circle");
+                    $("#"+data).addClass( "cross" );
+                }
+                else{
+                    alert("La casilla está ocupada, por favor escoja otra")
+                }
+            }
 
+        });
     });
 });
